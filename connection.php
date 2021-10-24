f839cdcddbcf044d5b810bbaf6dd92307346b481
Siphephelo Mlungwana <51417509+SpheITguru@users.noreply.github.com>
2021-10-25 00:53:12 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php 
 
-    $con = mysqli_connect(getenv("DB_HOST"),getenv("DB_USER"),getenv("DB_PASSWORD"),'sigmadb');
+    $con = mysqli_connect(getenv("DB_HOST"),getenv("DB_USER"),getenv("DB_PASSWORD"),'projectdb');
 
     if(!$con)
     {
