a293cf1a4a891ccb9014b6e194ab3cd6afde7d62
Siphephelo Mlungwana <51417509+SpheITguru@users.noreply.github.com>
2021-10-25 00:46:20 +0200

    Update signup.php

--- signup.php
+++ signup.php
@@ -17,9 +17,9 @@
         <input type="password" placeholder="Password">
         <input type="password" placeholder="Confirm Password">
         <div>
-            <input type="radio" id="student" name="users"><label for="student">Student</label>
-            <input type="radio" id="lecture" name="users"><label for="lecture">Lecture</label>
-            <input type="radio" id="sponsor" name="users"><label for="sponsor">Sponsor</label>
+            <input type="radio" id="student" name="role"><label for="student">Student</label>
+            <input type="radio" id="lecture" name="role"><label for="lecture">Lecture</label>
+            <input type="radio" id="sponsor" name="role"><label for="sponsor">Sponsor</label>
         </div>
         <input type="submit" name="Submit">
         <div class="switch">
