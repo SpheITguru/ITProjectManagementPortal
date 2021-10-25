b0065aa0bb4ad5452bbe2f80f06b120977ca448b
Siphephelo Mlungwana <51417509+SpheITguru@users.noreply.github.com>
2021-10-25 03:41:04 +0200

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,74 +1,22 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css" integrity="sha384-jLKHWM3JRmfMU0A5x5AkjWkw/EYfGUAGagvnfryNV3F9VqM98XiIH7VBGVoxVSc7" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/header.css">
-    <link rel="stylesheet" href="css/footer.css">
-	<link rel="stylesheet" href="css/index.css">
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
-    <title>IT Project Portal</title>
+    <style>
+    body{
+        text-align: center;
+    }
+    </style>
 </head>
-<body> 
-    <header>
-        <a href="#" class="logo">IT Portal</a>
-        <ul class="navigation">
-            <li><a href="index.php">Home</a></li>
-            <li><a href="about.php">About</a></li>
-            <li><a href="#">Dashboard</a></li>
-            <li><a href="contact.php">Contact Us</a></li>
-        </ul>
-        <ul class="login">
-            <li><a href="login.php">Login</a></li>
-            <li><a href="signup.php">Sign up</a></li>
-        </ul>
-    </header>
-    
+<body>
 
-	
-
-    <footer class="footer">
-  	 <div class="container">
-  	 	<div class="row">
-  	 		<div class="footer-col">
-  	 			<h4>company</h4>
-  	 			<ul>
-  	 				<li><a href="#">about us</a></li>
-  	 				<li><a href="#">testimonials</a></li>
-  	 				<li><a href="#">privacy policy</a></li>
-  	 			</ul>
-  	 		</div>
-  	 		<div class="footer-col">
-  	 			<h4>get help</h4>
-  	 			<ul>
-  	 				<li><a href="#">FAQ</a></li>
-  	 				<li><a href="#">Weekly Updates</a></li>
-  	 			</ul>
-  	 		</div>
-  	 		<div class="footer-col">
-  	 			<h4>Our History</h4>
-  	 			<ul>
-  	 				<li><a href="#">Projects</a></li>
-  	 				<li><a href="#">Top 10 students</a></li>
-  	 				<li><a href="#">Best sponsors</a></li>
-  	 				<li><a href="#">About the Lecture</a></li>
-  	 			</ul>
-  	 		</div>
-  	 		<div class="footer-col">
-  	 			<h4>follow us</h4>
-  	 			<div class="social-links">
-  	 				<a href="#"><i class="fab fa-facebook-f"></i></a>
-  	 				<a href="#"><i class="fab fa-twitter"></i></a>
-  	 				<a href="#"><i class="fab fa-instagram"></i></a>
-  	 				<a href="#"><i class="fab fa-linkedin-in"></i></a>
-  	 			</div>
-  	 		</div>
-  	 	</div>
-  	 </div>
-  </footer>
+    <h2>Welcom <?php echo $_SESSION['Username']; ?></h2>
+    Click here to <a href = "logout.php">Logout</a>
     
 </body>
 </html>
