fdca0dc714a961c606202fca5e418c91c87c0d79
Siphephelo Mlungwana <51417509+SpheITguru@users.noreply.github.com>
2021-10-25 02:49:03 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,3 +1,29 @@
+<?php
+$host = $_ENV["DB_HOST"];
+$username = $_ENV["DB_USER"];
+$password = $_ENV["DB_PASSWORD"];
+$db_name = "projectdb";
+
+mysqli_connect($host,$user,$password);
+mysqli_select_db(db_name);
+
+if(isset(['email'])){
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+    
+    $sql="select * from users where email='".$email."'AND pass='".$password."' limit 1";
+    
+    $result=mysqli_query($sql);
+    if(mysqli_num_rows($result)==1){
+        echo "You have Successfully logged in";
+        exit();
+    }
+    else{
+        echo " You have Entered Incorrect Password";
+    }
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,10 +35,10 @@
 </head>
 <body>
     
-    <form>
+    <form method="POST" action="#">
         <h1>Login</h1>
-        <input type="email" placeholder="Email">
-        <input type="password" placeholder="Password">
+        <input type="email" name="email" placeholder="Email">
+        <input type="password" name="password" placeholder="Password">
         <input type="submit" name="Submit">
         <div class="switch">
             <span>Don't you have an account? <a href="signup.php">Create Account</a></span>
