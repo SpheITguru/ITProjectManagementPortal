fb5db0fe58feebeb4db9613f901dd55a54aa1c9b
Siphephelo Mlungwana <51417509+SpheITguru@users.noreply.github.com>
2021-10-25 04:07:08 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,16 +1,9 @@
 <?php
+
 session_start();
-
-$host = $_ENV["DB_HOST"];
-$username = $_ENV["DB_USER"];
-$password = $_ENV["DB_PASSWORD"];
-
-$db_name = 'projectdb';
-
-$conn =  mysqli_connect('$host', '$username', '$password','$db_name') or die ('Unable to connect');
+$conn = mysqli_connect($_ENV["DB_HOST"], $_ENV["DB_USER"], $_ENV["DB_PASSWORD"] , 'projectdb') or die ('Unable to connect');
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
