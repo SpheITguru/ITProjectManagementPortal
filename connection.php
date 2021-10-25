530bc4af5e6114f75d700dfce199b5f1d4cec384
Siphephelo Mlungwana <51417509+SpheITguru@users.noreply.github.com>
2021-10-25 21:28:36 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,7 +1,6 @@
 <?php 
 
-    //$con = mysqli_connect(getenv("DB_HOST"),getenv("DB_USER"),getenv("DB_PASSWORD"),'projectdb');
-    $con = mysqli_connect(getenv("DB_HOST"),"admin1",getenv("DB_PASSWORD"),'projectdb');
+    $con = mysqli_connect(getenv("DB_HOST"),getenv("DB_USER"),getenv("DB_PASSWORD"),'projectdb');
 
     if(!$con)
     {
