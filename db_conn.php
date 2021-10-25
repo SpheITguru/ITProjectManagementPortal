d4c42e0f8ce04359dce7ce697166021ae89f5d38
Siphephelo Mlungwana <51417509+SpheITguru@users.noreply.github.com>
2021-10-25 12:44:59 +0200

    Update db_conn.php

--- db_conn.php
+++ db_conn.php
@@ -6,10 +6,10 @@ $password = $_ENV["DB_PASSWORD"];
 
 $db_name = "projectdb";
 
-$conn = new mysqli($host, $username, $password,$db_name);
+$con = new mysqli($host, $username, $password,$db_name);
 
 if ($conn->connect_error) {
-	die("Connection failed: " . $conn->connect_error);
+	die("Connection failed: " . $con->connect_error);
   }
   echo "Connected successfully";
 ?>
