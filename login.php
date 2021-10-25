1627fb70acc01d1c736f5e87087b29b2f32ca5c8
Siphephelo Mlungwana <51417509+SpheITguru@users.noreply.github.com>
2021-10-25 04:39:26 +0200

    Update login.php

--- login.php
+++ login.php
@@ -31,7 +31,7 @@ require_once('connection.php');
         $email = $_POST['email'];
         $pass = $_POST['password'];
 
-    $select = mysqli_query($con," SELECT * FROM users WHERE email = '$email' AND pass = '$pass' ");
+    $select = mysqli_query($con," SELECT * FROM users WHERE email = '".$email."' AND pass = '".$pass."' ");
     $row  = mysqli_fetch_array($select);
 
     if(is_array($row)) {
