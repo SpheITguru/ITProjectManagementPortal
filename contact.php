13d07918084928d686367a9852772eee736fba98
Siphephelo Mlungwana <51417509+SpheITguru@users.noreply.github.com>
2021-11-20 18:41:56 +0200

    jghjgh

--- contact.php
+++ contact.php
@@ -5,14 +5,14 @@
     $phone = $_POST["subject"];
     $message = $_POST["message"];
 
-    $to = $email;
+    $to = "sphe.mlu@gmail.com";
     $subject = $message;
 
     $message = "Name: {$username} Email: {$email} Subject: {$phone}  Message: " . $message;
 
     // Always set content-type when sending HTML email
-    $headers = "MIME-Version: 1.0" . "\r\n";
-    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    //$headers = "MIME-Version: 1.0" . "\r\n";
+    //$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
     // More headers
     $headers .= 'From: IT Portal';
