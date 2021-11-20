e1ff038d95836d299623ce40d1a732851e782497
Siphephelo Mlungwana <51417509+SpheITguru@users.noreply.github.com>
2021-11-20 18:34:49 +0200

    dfgdfhgdg

--- contact.php
+++ contact.php
@@ -12,10 +12,10 @@
 
     // Always set content-type when sending HTML email
     $headers = "MIME-Version: 1.0" . "\r\n";
-    $headers = "Content-type:text/html;charset=UTF-8" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
     // More headers
-    $headers = 'From: IT Portal';
+    $headers .= 'From: IT Portal';
 
     $mail = mail($to,$subject,$message,$headers);
 
