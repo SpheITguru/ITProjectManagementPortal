a24848be4b5c877b615017c0de5999c2a556ecc8
Siphephelo Mlungwana <51417509+SpheITguru@users.noreply.github.com>
2021-11-20 09:15:39 +0200

    remove post

--- contact.php
+++ contact.php
@@ -83,7 +83,7 @@
           <span class="circle one"></span>
           <span class="circle two"></span>
 
-          <form action="index.html" methode="POST"autocomplete="off">
+          <form action="index.html" autocomplete="off">
             <h3 class="title">Contact us</h3>
             <div class="input-container">
               <input type="text" name="name" class="input" />
