f0040f5fcd799078a56dc09ab54a67af877064bc
Siphephelo Mlungwana <51417509+SpheITguru@users.noreply.github.com>
2021-11-20 18:45:44 +0200

    jkljkl

--- contact.php
+++ contact.php
@@ -2,17 +2,17 @@
   if (isset($_POST["submit"])) {
     $username = $_POST["name"];
     $email = $_POST["email"];
-    $phone = $_POST["subject"];
+    $subject = $_POST["subject"];
     $message = $_POST["message"];
 
     $to = "sphe.mlu@gmail.com";
-    $subject = $message;
+    //$subject = $message;
 
-    $message = "Name: {$username} Email: {$email} Subject: {$phone}  Message: " . $message;
+    $message = "Name: {$username} Email: {$email} Subject: {$subject}  Message: " . $message;
 
     // Always set content-type when sending HTML email
-    //$headers = "MIME-Version: 1.0" . "\r\n";
-    //$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
     // More headers
     $headers .= 'From: IT Portal';
