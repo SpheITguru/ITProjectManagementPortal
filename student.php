05ae0a1cb0f25693637aa7951a72c946a0469348
Siphephelo Mlungwana <51417509+SpheITguru@users.noreply.github.com>
2021-11-23 07:48:25 +0200

    fsdfsd

--- student.php
+++ student.php
@@ -78,7 +78,7 @@ require_once('connection.php');
 	}
 	?>
 	<!-- This is a comment for left table -->
-    <h3>Project Team</h3>
+    <h3>Project Team <?php echo $_SESSION['team']; ?></h3>
 	<form class="form-inline" method="POST">
 		
 		<?php
